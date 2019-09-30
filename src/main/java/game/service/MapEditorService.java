065ac736778efdb02a2facaf53a6b2dbc714403c
Muzi Qiu <mu_qiu@encs.concordia.ca>
2019-09-29 22:17:50 -0400

    fix

--- src/main/java/game/service/MapEditorService.java
+++ src/main/java/game/service/MapEditorService.java
@@ -3,6 +3,7 @@ package service;
 import model.MapGraph;
 
 import java.io.File;
+import java.io.IOException;
 
 public class MapEditorService {
 
@@ -15,6 +16,14 @@ public class MapEditorService {
 
         } else {
             File file = new File(fileName);
+            try {
+                //create an empty file
+                file.createNewFile();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
