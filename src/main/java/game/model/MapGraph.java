bb825e25d30423d607baf0e2f5299ca153debb03
siming477 <hsim47@163.com>
2019-10-01 13:55:36 -0400

    add more functions

--- src/main/java/game/model/MapGraph.java
+++ src/main/java/game/model/MapGraph.java
@@ -4,6 +4,40 @@ import java.util.List;
 import java.util.Map;
 
 public class MapGraph {
+    String name;
+
+    Integer height, width;
 
     private Map<Country, List<Country>> adjacentCountries;
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setHeight(Integer height)
+    {
+        this.height=height;
+    }
+
+    public void setWidth(Integer width)
+    {
+        this.width=width;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public Integer getHeight()
+    {
+        return this.height;
+    }
+
+    public Integer getWidth()
+    {
+        return this.width;
+    }
+
 }
