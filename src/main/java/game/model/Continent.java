c461b076d72026c30a8d996441cbb867f2abb149
siming477 <hsim47@163.com>
2019-10-01 12:58:23 -0400

    add more functions

--- src/main/java/game/model/Continent.java
+++ src/main/java/game/model/Continent.java
@@ -6,7 +6,123 @@ public class Continent {
 
     String continentName;
 
-    int controlValue;
+    static Integer countryNo;
+
+    Integer armyValue;
 
     List<Country> countries;
+
+    /**
+     * add continent
+     * @param continentName
+     * @param armyValue
+     */
+    public void addContinent(String continentName, int armyValue)
+    {
+        this.continentName=continentName;
+        this.armyValue=armyValue;
+    }
+
+    /**
+     * remove continent
+     * @param continentName
+     */
+    public void removeContinent(String continentName)
+    {
+
+    }
+
+    /**
+     * edit continent
+     * @param continentName
+     * @param armyValue
+     */
+    public void editContinent(String continentName, int armyValue)
+    {
+        this.continentName=continentName;
+        this.armyValue=armyValue;
+    }
+
+    /**
+     * add country to a continent
+     * @param country
+     */
+    public void addCountry(Country country)
+    {
+        if(!this.countries.contains(country))
+        {
+            countryNo++;
+            this.countries.add(country);
+        }
+    }
+
+    /**
+     * remove country from a continent
+     * @param country
+     */
+    public void removeCountry(Country country)
+    {
+        if(this.countries.contains(country))
+        {
+            countryNo--;
+            this.countries.remove(country);
+        }
+    }
+
+    /**
+     * edit country
+     * @param countryName
+     * @param parentContinent
+     */
+    public void editCountry(String countryName, Continent parentContinent)
+    {
+
+    }
+
+    /**
+     * check if a country is belong to a continent
+     * @param country
+     * @return
+     */
+    public boolean includeCountry(Country country)
+    {
+        return this.countries.contains(country);
+    }
+
+    /**
+     * get continent name
+     * @return
+     */
+    public String getContinentName()
+    {
+        return continentName;
+    }
+
+    /**
+     * get army value
+     * @return
+     */
+    public Integer getArmyValue()
+    {
+        return armyValue;
+    }
+
+    /**
+     * get country number
+     * @return
+     */
+    public Integer getCountryNo()
+    {
+        return countryNo;
+    }
+
+    /**
+     * get country list of a continent
+     * @return
+     */
+    public List<Country> getCountries()
+    {
+        return countries;
+    }
+
 }
--- src/main/java/game/model/Country.java
+++ src/main/java/game/model/Country.java
@@ -14,4 +14,58 @@ public class Country {
 
     Integer armyValue;
 
+    /**
+     * initialise class member
+     * @param countryName
+     */
+    public Country(String countryName)
+    {
+        this.countryName=countryName;
+    }
+
+    /**
+     * add neighbor
+     * @param country
+     */
+    public void addNeighbor(Country country)
+    {
+        this.neighbours.add(country);
+    }
+
+    /**
+     * remove neighbor
+     * @param country
+     */
+    public void removeNeighbor(Country country)
+    {
+        this.neighbours.remove(country);
+    }
+
+    /**
+     * check if countries are connected
+     * @param country
+     * @return
+     */
+    public boolean connected(Country country)
+    {
+        return this.neighbours.contains(country);
+    }
+
+    /**
+     * get country name
+     * @return
+     */
+    public String getCountryName()
+    {
+        return countryName;
+    }
+
+    /**
+     * get all neighbours of a country
+     * @return
+     */
+    public List<Country> getNeighbours()
+    {
+        return neighbours;
+    }
 }
