5882be4ef4d68124ab0512eb24b526f281975192
Muzi Qiu <mu_qiu@encs.concordia.ca>
2019-10-19 15:20:28 -0400

    add phase

--- src/main/java/game/service/CommandService.java
+++ src/main/java/game/service/CommandService.java
@@ -1,6 +1,9 @@
 package service;
 
 public class CommandService {
+
+    private Phase phase;
+
     /**
      * Prompt Command into console
      *
@@ -16,57 +19,73 @@ public class CommandService {
         inputCommand = inputCommand.trim();
         String commandReturnMsg = "";
 
-            if (inputCommand.startsWith("editmap") || inputCommand.startsWith("loadmap")) {
-                String[] arguments = inputCommand.split(" ");
-                commandReturnMsg = mapEditorService.editMap(arguments[1]);
-            } else if (inputCommand.startsWith("showmap")) {
-                commandReturnMsg = mapEditorService.showMap();
-            } else if (inputCommand.startsWith("savemap")) {
-                String[] arguments = inputCommand.split(" ");
-                commandReturnMsg = mapEditorService.saveMap(arguments[1]);
-            } else if (inputCommand.startsWith("validatemap")) {
-                if (!mapEditorService.validateMap()) {
-                    return "the map is not valid";
-                } else {
-                    return "the map is valid";
-                }
-            } else if (inputCommand.startsWith("editcontinent")) {
-                String[] arguments = inputCommand.split(" ");
-                commandReturnMsg = mapEditorService.editContinent(arguments);
-            } else if (inputCommand.startsWith("editcountry")) {
-                String[] arguments = inputCommand.split(" ");
-                commandReturnMsg = mapEditorService.editCountry(arguments);
-            } else if (inputCommand.startsWith("editneighbor")) {
-                String[] arguments = inputCommand.split(" ");
-                commandReturnMsg = mapEditorService.editNeighbor(arguments);
-            } else if (inputCommand.startsWith("gameplayer")) {
-                String[] arguments = inputCommand.split(" ");
-                commandReturnMsg = gamePlayerService.gamePlayerAction(arguments);
-            } else if (inputCommand.startsWith("populatecountries")) {
-                String populateReturnMsg = gamePlayerService.populateCountries();
-                String alloReturnMsg = gamePlayerService.alloInitialArmy();
-                commandReturnMsg = populateReturnMsg + " " + alloReturnMsg;
-            } else if (inputCommand.startsWith("placearmy")) {
-                String[] arguments = inputCommand.split(" ");
-                commandReturnMsg = gamePlayerService.placeOneArmy(arguments[1]);
-            } else if (inputCommand.startsWith("placeall")) {
-                String[] arguments = inputCommand.split(" ");
-                commandReturnMsg = gamePlayerService.placeAll();
-            } else if (inputCommand.startsWith("start")) {
-                commandReturnMsg = gamePlayerService.calReinArmyNum();
-            } else if (inputCommand.startsWith("reinforce")) {
-                String[] arguments = inputCommand.split(" ");
-                commandReturnMsg = reinforceService.reinforce(arguments[1], arguments[2]);
-            } else if (inputCommand.startsWith("fortify")) {
-                String[] arguments = inputCommand.split(" ");
-                if (arguments[1].startsWith("none")) {
-                    commandReturnMsg = fortifyService.fortifyNone();
-                } else {
-                    commandReturnMsg = fortifyService.fortify(arguments[1], arguments[2], arguments[3]);
-                }
+        switch (phase) {
+            case MAPEDITOR:
+                // code block
+                break;
+            case GAMEPLAY:
+                // code block
+                break;
+            case STARTUP:
+                break;
+            case REINFORCEMENT:
+                break;
+            case FORTIFICATION:
+                break;
+            default:
+                // code block
+        }
+        if (inputCommand.startsWith("editmap") || inputCommand.startsWith("loadmap")) {
+            String[] arguments = inputCommand.split(" ");
+            commandReturnMsg = mapEditorService.editMap(arguments[1]);
+        } else if (inputCommand.startsWith("showmap")) {
+            commandReturnMsg = mapEditorService.showMap();
+        } else if (inputCommand.startsWith("savemap")) {
+            String[] arguments = inputCommand.split(" ");
+            commandReturnMsg = mapEditorService.saveMap(arguments[1]);
+        } else if (inputCommand.startsWith("validatemap")) {
+            if (!mapEditorService.validateMap()) {
+                return "the map is not valid";
             } else {
-                return "wrong syntax";
+                return "the map is valid";
             }
+        } else if (inputCommand.startsWith("editcontinent")) {
+            String[] arguments = inputCommand.split(" ");
+            commandReturnMsg = mapEditorService.editContinent(arguments);
+        } else if (inputCommand.startsWith("editcountry")) {
+            String[] arguments = inputCommand.split(" ");
+            commandReturnMsg = mapEditorService.editCountry(arguments);
+        } else if (inputCommand.startsWith("editneighbor")) {
+            String[] arguments = inputCommand.split(" ");
+            commandReturnMsg = mapEditorService.editNeighbor(arguments);
+        } else if (inputCommand.startsWith("gameplayer")) {
+            String[] arguments = inputCommand.split(" ");
+            commandReturnMsg = gamePlayerService.gamePlayerAction(arguments);
+        } else if (inputCommand.startsWith("populatecountries")) {
+            String populateReturnMsg = gamePlayerService.populateCountries();
+            String alloReturnMsg = gamePlayerService.alloInitialArmy();
+            commandReturnMsg = populateReturnMsg + " " + alloReturnMsg;
+        } else if (inputCommand.startsWith("placearmy")) {
+            String[] arguments = inputCommand.split(" ");
+            commandReturnMsg = gamePlayerService.placeOneArmy(arguments[1]);
+        } else if (inputCommand.startsWith("placeall")) {
+            String[] arguments = inputCommand.split(" ");
+            commandReturnMsg = gamePlayerService.placeAll();
+        } else if (inputCommand.startsWith("start")) {
+            commandReturnMsg = gamePlayerService.calReinArmyNum();
+        } else if (inputCommand.startsWith("reinforce")) {
+            String[] arguments = inputCommand.split(" ");
+            commandReturnMsg = reinforceService.reinforce(arguments[1], arguments[2]);
+        } else if (inputCommand.startsWith("fortify")) {
+            String[] arguments = inputCommand.split(" ");
+            if (arguments[1].startsWith("none")) {
+                commandReturnMsg = fortifyService.fortifyNone();
+            } else {
+                commandReturnMsg = fortifyService.fortify(arguments[1], arguments[2], arguments[3]);
+            }
+        } else {
+            return "wrong syntax";
+        }
 
         return commandReturnMsg;
     }
---
+++ src/main/java/game/service/Phase.java
@@ -0,0 +1,9 @@
+package service;
+
+public enum Phase {
+    MAPEDITOR,
+    GAMEPLAY,
+    STARTUP,
+    REINFORCEMENT,
+    FORTIFICATION
+}
