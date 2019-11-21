a5164c6908aafd804cfb7d0e16d913f76de43d59
wujq95 <wujq1214@mails.jlu.edu.cn>
2019-11-21 15:23:25 -0500

    add benevolent and aggressive pattern

--- src/main/java/game/strategy/AggressiveStrategy.java
+++ src/main/java/game/strategy/AggressiveStrategy.java
@@ -1,5 +1,9 @@
 package strategy;
 
+import model.GamePlayer;
+import service.GamePlayerService;
+import service.MapEditorService;
+
 public class AggressiveStrategy implements Strategy {
     @Override
     public void attack() {
@@ -8,6 +12,25 @@ public class AggressiveStrategy implements Strategy {
 
     @Override
     public void reinforce() {
+
+        GamePlayerService gamePlayerService = new GamePlayerService();
+        gamePlayerService.calReinArmyNum();
+        GamePlayer player = GamePlayerService.playerList.get(GamePlayerService.choosePlayer);
+        Integer armyValue = player.getArmyValue();
+        Integer armyFlag = MapEditorService.mapGraph.getCountryList().get(0).getArmyValue();
+        Integer countryFlag = 0;
+        for(int i=0;i< MapEditorService.mapGraph.getCountryList().size();i++){
+            if(MapEditorService.mapGraph.getCountryList().get(i).getPlayer().getPlayerName().equals(player.getPlayerName())){
+                if(MapEditorService.mapGraph.getCountryList().get(i).getArmyValue()>armyFlag){
+                    armyFlag = MapEditorService.mapGraph.getCountryList().get(i).getArmyValue();
+                    countryFlag = i;
+                }
+            }
+        }
+
+        MapEditorService.mapGraph.getCountryList().get(countryFlag).setArmyValue(armyFlag+armyValue);
+        GamePlayerService.playerList.get(GamePlayerService.choosePlayer).setArmyValue(0);
+
         System.out.println("Reinforcing aggressively...");
     }
 
--- src/main/java/game/strategy/BenevolentStrategy.java
+++ src/main/java/game/strategy/BenevolentStrategy.java
@@ -1,5 +1,9 @@
 package strategy;
 
+import model.GamePlayer;
+import service.GamePlayerService;
+import service.MapEditorService;
+
 public class BenevolentStrategy implements Strategy {
     @Override
     public void attack() {
@@ -8,11 +12,29 @@ public class BenevolentStrategy implements Strategy {
 
     @Override
     public void reinforce() {
+        GamePlayerService gamePlayerService = new GamePlayerService();
+        gamePlayerService.calReinArmyNum();
+        GamePlayer player = GamePlayerService.playerList.get(GamePlayerService.choosePlayer);
+        Integer armyValue = player.getArmyValue();
+        Integer armyFlag = MapEditorService.mapGraph.getCountryList().get(0).getArmyValue();
+        Integer countryFlag = 0;
+        for(int i=0;i< MapEditorService.mapGraph.getCountryList().size();i++){
+            if(MapEditorService.mapGraph.getCountryList().get(i).getPlayer().getPlayerName().equals(player.getPlayerName())){
+                if(MapEditorService.mapGraph.getCountryList().get(i).getArmyValue()<armyFlag){
+                    armyFlag = MapEditorService.mapGraph.getCountryList().get(i).getArmyValue();
+                    countryFlag = i;
+                }
+            }
+        }
+
+        MapEditorService.mapGraph.getCountryList().get(countryFlag).setArmyValue(armyFlag+armyValue);
+        GamePlayerService.playerList.get(GamePlayerService.choosePlayer).setArmyValue(0);
         System.out.println("Reinforcing benevolently...");
     }
 
     @Override
     public void fortify() {
+
         System.out.println("Fortifying benevolently...");
     }
 }
