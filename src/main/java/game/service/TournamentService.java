6d24961ae5039fe6c90b5d44ded8d7e870c35151
wujq95 <wujq1214@mails.jlu.edu.cn>
2019-11-27 20:48:43 -0500

    tournament service parameter

--- src/main/java/game/service/TournamentService.java
+++ src/main/java/game/service/TournamentService.java
@@ -35,99 +35,162 @@ public class TournamentService extends Observable {
      * @return result
      */
     public String tournament(String[] arguments){
-        List<String> mapFileList = new LinkedList<>();
-        List<String> playerStrategyList = new LinkedList<>();
-        GamePlayerService gamePlayerService = new GamePlayerService();
-        gamePlayerService.setObserver(observer);
-        Integer gameNumber =0;
-        Integer maxTurnNumber = 0;
-        Integer indexP =0;
-        Integer indexG = 0;
-        boolean flag = true;
         String result = "game result:\n";
-        for(int i=0;i<arguments.length;i++){
-            if(arguments[i].equals("-P")){
-                indexP = i;
+        boolean flagT = true;
+        try{
+            List<String> mapFileList = new LinkedList<>();
+            List<String> playerStrategyList = new LinkedList<>();
+            GamePlayerService gamePlayerService = new GamePlayerService();
+            gamePlayerService.setObserver(observer);
+            Integer gameNumber =0;
+            Integer maxTurnNumber = 0;
+            Integer indexP =0;
+            Integer indexG = 0;
+            boolean flag = true;
+            for(int i=0;i<arguments.length;i++){
+                if(arguments[i].equals("-P")){
+                    indexP = i;
+                }
+                if(arguments[i].equals("-G")){
+                    indexG = i;
+                }
             }
-            if(arguments[i].equals("-G")){
-                indexG = i;
+            for(int i=2;i<indexP;i++){
+                mapFileList.add(arguments[i]);
             }
-        }
-        for(int i=2;i<indexP;i++){
-            mapFileList.add(arguments[i]);
-        }
-        for(int i=indexP+1;i<indexG;i++){
-            playerStrategyList.add(arguments[i]);
-        }
-        gameNumber = Integer.parseInt(arguments[indexG+1]);
-        maxTurnNumber = Integer.parseInt(arguments[indexG+3]);
+            for(int i=indexP+1;i<indexG;i++){
+                playerStrategyList.add(arguments[i]);
+            }
+            gameNumber = Integer.parseInt(arguments[indexG+1]);
+            maxTurnNumber = Integer.parseInt(arguments[indexG+3]);
 
-        Integer index = 1;
-        for(int i=0;i<gameNumber;i++){
-            for(int j=0;j<mapFileList.size();j++){
-                result = result+"Game"+(i+1);
-                result = result+" Map"+(j+1);
-                mapEditorService.editMap(mapFileList.get(j));
-                for(int t=0;t<playerStrategyList.size();t++){
-                    String playerName = "test"+(t+1);
-                    if(playerStrategyList.get(t).equals("Aggressive")){
-                        Strategy strategy = new AggressiveStrategy();
-                        gamePlayerService.addPlayer(playerName,strategy);
-                    }else if(playerStrategyList.get(t).equals("Random")){
-                        Strategy strategy = new RandomStrategy();
-                        gamePlayerService.addPlayer(playerName,strategy);
-                    }else if(playerStrategyList.get(t).equals("Benevolent")){
-                        Strategy strategy = new BenevolentStrategy();
-                        gamePlayerService.addPlayer(playerName,strategy);
-                    }else if(playerStrategyList.get(t).equals("Cheater")){
-                        Strategy strategy = new CheaterStrategy();
-                        gamePlayerService.addPlayer(playerName,strategy);
-                    }else{
-                        flag = false;
+            flagT = checkStrategyNumber(playerStrategyList);
+
+            Integer mapNumber  = indexP-2;
+            if(mapNumber<=0||mapNumber>5||mapNumber % 1 != 0){
+                flagT = false;
+            }
+            if(gameNumber<=0||gameNumber>5||gameNumber % 1 != 0){
+                flagT = false;
+            }
+            if(maxTurnNumber<10||maxTurnNumber>50||maxTurnNumber % 1 != 0){
+                flagT = false;
+            }
+            if(flagT){
+                for(int i=0;i<gameNumber;i++) {
+                    for (int j = 0; j < mapFileList.size(); j++) {
+                        result = result + "Game" + (i + 1);
+                        result = result + " Map" + (j + 1);
+                        mapEditorService.editMap(mapFileList.get(j));
+                        for (int t = 0; t < playerStrategyList.size(); t++) {
+                            String playerName = "test" + (t + 1);
+                            if (playerStrategyList.get(t).equals("Aggressive")) {
+                                Strategy strategy = new AggressiveStrategy();
+                                gamePlayerService.addPlayer(playerName, strategy);
+                            } else if (playerStrategyList.get(t).equals("Random")) {
+                                Strategy strategy = new RandomStrategy();
+                                gamePlayerService.addPlayer(playerName, strategy);
+                            } else if (playerStrategyList.get(t).equals("Benevolent")) {
+                                Strategy strategy = new BenevolentStrategy();
+                                gamePlayerService.addPlayer(playerName, strategy);
+                            } else if (playerStrategyList.get(t).equals("Cheater")) {
+                                Strategy strategy = new CheaterStrategy();
+                                gamePlayerService.addPlayer(playerName, strategy);
+                            } else {
+                                flag = false;
+                            }
+                        }
+                        if (flag) {
+                            gamePlayerService.populateCountries();
+                            gamePlayerService.alloInitialArmy();
+                            gamePlayerService.placeAll();
+
+                            for (int p = 0; p < maxTurnNumber; p++) {
+                                boolean flag3 = true;
+                                while (flag3) {
+                                    GamePlayer player = GamePlayerService.playerList.get(GamePlayerService.choosePlayer);
+                                    player.reinforce();
+                                    player.attack();
+                                    player.fortify();
+                                    boolean flag2 = false;
+                                    if (GamePlayerService.choosePlayer >= GamePlayerService.playerList.size() - 1) {
+                                        flag2 = true;
+                                    }
+                                    if (flag2) {
+                                        GamePlayerService.choosePlayer = 0;
+                                        flag3 = false;
+                                    } else {
+                                        GamePlayerService.choosePlayer++;
+                                    }
+                                }
+                                if (GamePlayerService.playerList.size() == 1) {
+                                    break;
+                                }
+                            }
+                            if (GamePlayerService.playerList.size() == 1) {
+                                for (int p = 0; p < GamePlayerService.playerList.size(); p++) {
+                                    result = result + " result: " + GamePlayerService.playerList.get(p).getStrategyName() + "\n";
+                                }
+                            } else {
+                                result = result + " result: Draw\n";
+                            }
+
+                            for (int p = GamePlayerService.playerList.size() - 1; p >= 0; p--) {
+                                GamePlayerService.playerList.remove(p);
+                            }
+                        }
+
                     }
                 }
-                if(flag){
-                    gamePlayerService.populateCountries();
-                    gamePlayerService.alloInitialArmy();
-                    gamePlayerService.placeAll();
-
-                    for(int p=0;p<maxTurnNumber;p++){
-                        boolean flag3 = true;
-                        while(flag3){
-                            GamePlayer player = GamePlayerService.playerList.get(GamePlayerService.choosePlayer);
-                            player.reinforce();
-                            player.attack();
-                            player.fortify();
-                            boolean flag2 = false;
-                            if (GamePlayerService.choosePlayer >= GamePlayerService.playerList.size() - 1) {
-                                flag2 = true;
-                            }
-                            if(flag2){
-                                GamePlayerService.choosePlayer=0;
-                                flag3 = false;
-                            }else{
-                                GamePlayerService.choosePlayer++;
-                            }
-                        }
-                        if(GamePlayerService.playerList.size()==1){
-                            break;
-                        }
-                    }
-                    if(GamePlayerService.playerList.size()==1){
-                        for(int p=0;p<GamePlayerService.playerList.size();p++){
-                            result = result+" result: "+GamePlayerService.playerList.get(p).getStrategyName()+"\n";
-                        }
-                    }else {
-                        result = result+" result: Draw\n";
-                    }
-
-                    for(int p=GamePlayerService.playerList.size()-1;p>=0;p--){
-                        GamePlayerService.playerList.remove(p);
-                    }
-                }
-                index++;
+            }else{
+                result = "parameter setting error";
             }
+        }catch(Exception e){
+            result = "wrong syntax";
         }
         return result;
+    }
+
+    /**
+     * check if the number of strategy meets the requirement
+     * @param aList
+     * @return
+     */
+    public boolean checkStrategyNumber(List<String> aList){
+        boolean flag = true;
+        boolean aggressive = false;
+        boolean cheater = false;
+        boolean benevolent = false;
+        boolean random = false;
+        Integer result = 0;
+        for(String s:aList){
+            if(s.equals("Aggressive")){
+                aggressive = true;
+            }else if(s.equals("Random")){
+                cheater = true;
+            }else if(s.equals("Benevolent")){
+                benevolent = true;
+            }else if(s.equals("Cheater")){
+                random = true;
+            }
+        }
+        if(aggressive==true){
+            result++;
+        }
+        if(random==true){
+            result++;
+        }
+        if(benevolent==true){
+            result++;
+        }
+        if(cheater==true){
+            result++;
+        }
+        if(result<2){
+            flag=false;
+        }
+
+
+        return  flag;
     }
 }
